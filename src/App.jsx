c69a773a4a656ec05b420615b0c7397f89a44a3b
Fabrizio Mirabella <fmirabella08@gmail.com>
2025-04-15 13:20:46 -0400

    finished exercise 5.1 - step 1

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from 'react'
+import { Blog, Notification } from './components'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    // Handle login logic here
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000);
+    }
+  }
+  
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const blogs = await blogService.getAll();
+        setBlogs(blogs);
+      } catch (error) {
+        console.error('Error fetching blogs:', error);
+      }
+    }
+    fetchBlogs()
+  }, [])
+
+  const LoginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+            />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+            />
+        </div>
+        <button type="submit">login</button>
+      </form> 
+    </div>
+  )
+
+  // const blogsForm = () => (
+  //   <form onSubmit={addBlog}>
+  //     <input
+  //       value={newBlog}
+  //       onChange={handleBlogChange}
+  //     />
+  //     <button type="submit">save</button>
+  //   </form>  
+  // )
+
+
+  return (
+    <div>
+      {/* <h1>Blogs</h1> */}
+      
+      <Notification message={errorMessage} />
+
+      {user === null 
+        ? LoginForm() 
+        : <div>
+            <h2>Blogs</h2>
+            {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+          </div>
+      }
+
+    </div>
+  )
+}
+
+export default App
